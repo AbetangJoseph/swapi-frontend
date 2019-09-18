24b55c2837390ec9eff7ad3a63019a723f5dc3e1
Abetang Joseph <josephabetang@gmail.com>
2019-09-18 20:02:15 +0100

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,110 @@
+<template>
+  <a
+    href="#"
+    id="btn"
+    class="btn"
+    :style="style"
+    @mouseover="mouseOver()"
+    @mouseleave="mouseLeave()"
+  >{{btnText }}</a>
+</template>
+
+
+<script>
+export default {
+  name: "button",
+  props: {
+    bgcolor: {
+      type: String,
+      default: "#fff"
+    },
+    width: {
+      type: Number,
+      default: 150
+    },
+    borderRadius: {
+      type: Number,
+      default: 0
+    },
+    btnText: {
+      type: String,
+      default: "View More"
+    },
+    border: {
+      type: String,
+      default: "none"
+    },
+    fontSize: {
+      type: Number,
+      default: 12
+    },
+    icon: {
+      type: String
+    }
+  },
+  computed: {
+    style() {
+      return {
+        "background-color": this.bgColorHover,
+        width: `${this.width}px`,
+        borderRadius: `${this.borderRadius}px`,
+        border: this.border,
+        fontSize: `${this.fontSize}px`
+      };
+    }
+  },
+  data() {
+    return {
+      bgColorHover: this.bgcolor
+    };
+  },
+  methods: {
+    mouseOver() {
+      this.bgColorHover = "black";
+    },
+    mouseLeave() {
+      this.bgColorHover = this.bgcolor;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* .btn {
+  border-radius: 0px;
+  background-color: rgb(214, 214, 214);
+  float: right;
+  height: 45px;
+  padding: 12px;
+  width: 150px;
+  justify-content: center;
+  font-size: 12px;
+  font-family: "Montserrat", sans-serif;
+}
+
+.btn:hover {
+  background-color: black;
+  color: white;
+} */
+
+.btn {
+  border-radius: 0px;
+  /* background-color: rgb(214, 214, 214); */
+  float: right;
+  height: 45px;
+  padding: 12px;
+  width: 150px;
+  justify-content: center;
+  /* font-size: 12px; */
+  font-family: "Montserrat", sans-serif;
+}
+
+#btn:hover {
+  background-color: black;
+  color: white;
+}
+
+img {
+  fill: yellow;
+}
+</style>
